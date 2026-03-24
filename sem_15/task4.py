e18ecdda2e794a3faf44b823b5907ac1fbf23f1a
vnj64 <kamil_khabibullin@bk.ru>
2026-03-24 12:19:27 +0300

    решил задачу номер 4 на уровень хакеров

---
+++ sem_15/task4.py
@@ -0,0 +1,13 @@
+def hackers_func(hackers: list, security_level: int, increase: int) -> list:
+    if len(hackers) == 0:
+        return 0
+    success = 0
+    for level in hackers:
+        if level <= security_level:
+            security_level += increase
+        else:
+            success += 1
+    
+    return success
+
+print(hackers_func([10, 8, 3, 11, 24], 8, 1))
